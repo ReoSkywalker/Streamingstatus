6849b923baaee6de975b8e4678fead21b540eceb
ReoSkywalker <86115564+ReoSkywalker@users.noreply.github.com>
2021-07-07 23:04:31 -0700

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ client = commands.Bot(
 
 @client.event
 async def on_connect():
-  await client.change_presence(activity = discord.Streaming(name = "24/7 Online ┊ ReoGanteng ┊ 7/30 Hari lagi.", url = "https://www.twitch.tv/realreoskywalker"))
+  await client.change_presence(activity = discord.Streaming(name = "24/7 Online ┊ ReoGanteng", url = "https://www.twitch.tv/realreoskywalker"))
 
 
 keep_alive.keep_alive()
